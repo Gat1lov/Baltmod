e7cdd755b4f74cb014d5476c123b02989f4f5141
Gat1lov <gatila5d5@yandex.ru>
2024-05-07 11:41:36 +0300

    2| Update Files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-//Массив фотографий для проектов
-const chapel = [
-    { src: "./images/chasovnya2.jpg", alt: "Часовня2" },
-    { src: "./images/chasovnya3.jpg", alt: "Часовня3" },
-    { src: "./images/chasovnya4.jpg", alt: "Часовня4" },
-    { src: "./images/chasovnya5.jpg", alt: "Часовня5" }
-];
+import '../src/styles/index.css'
 
-const dino = [
-    { src: "./images/dinopark2.jpg", alt: "Динопарк2" },
-    { src: "./images/dinopark3.jpg", alt: "Динопарк3" },
-    { src: "./images/dinopark4.jpg", alt: "Динопарк4" },
-    { src: "./images/dinopark5.jpg", alt: "Динопарк5" }
-];
-
-const sport = [
-    { src: "./images/sport2.jpg", alt: "Спорт2" },
-    { src: "./images/sport3.jpg", alt: "Спорт3" },
-    { src: "./images/sport4.jpg", alt: "Спорт4" },
-    { src: "./images/sport5.jpg", alt: "Спорт5" }
-];
+import {chapel, dino, sport} from './components/cards';
 
 //Константы 
 const chapelContainer = document.getElementById('chasovnya');
